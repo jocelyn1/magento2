d6df678ab90cadec1ee2567976d4be6b9bee3e3f
jocelyn <jocelynkelle@gmail.com>
2017-04-10 15:13:21 +0200

    views in module with layout

--- app/code/Sbe/Contacts/Controller/Test/Index.php
+++ app/code/Sbe/Contacts/Controller/Test/Index.php
@@ -1,9 +1,13 @@
 <?php
+
 namespace Sbe\Contacts\Controller\Test;
+
 class Index extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
-        die('test index');
+        //die('test index');
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();
     }
 }
