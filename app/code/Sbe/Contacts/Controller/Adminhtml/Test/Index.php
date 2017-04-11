efbfb48c75539e895db8bb5cac0c1259cf90b484
jocelyn <jocelynkelle@gmail.com>
2017-04-11 15:28:58 +0200

    add menu, controller and routes admin

---
+++ app/code/Sbe/Contacts/Controller/Adminhtml/Test/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sbe\Contacts\Controller\Adminhtml\Test;
+
+use Magento\Backend\App\Action;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        die('test admin view');
+    }
+}
