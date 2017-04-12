dc7c23b39ef25e56c6b99784b503c842dfadd6f5
jocelyn <jocelynkelle@gmail.com>
2017-04-12 10:32:45 +0200

    test api client

--- apitest.php
+++ apitest.php
@@ -1,6 +1,6 @@
 <?php
 
-$adminUrl='http://www.magento2.lan/index.php/rest/V1/integration/admin/token';
+$adminUrl='http://www.magento2.lan/rest/V1/integration/admin/token';
 $ch = curl_init();
 $data = array("username" => "wsuser", "password" => "password123");
 $data_string = json_encode($data);
@@ -18,13 +18,15 @@ $token =  json_decode($token);
 //Use above token into header
 $headers = array("Authorization: Bearer $token");
 
-$requestUrl='http://www.magento2.lan/index.php/rest/V1/products/24-WG085';
+$requestUrl='http://www.magento2.lan/rest/V1/customers/1';
 
 $ch = curl_init();
 $ch = curl_init($requestUrl);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $result = curl_exec($ch);
-$result = json_decode($result);
-print_r($result);
+var_dump($result);
+/*$result = json_decode($result);
+print_r($result);*/
