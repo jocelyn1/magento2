06dca8d4d1faf87cdebd83d216105a5a70547de5
jocelyn <jocelynkelle@gmail.com>
2017-04-18 13:59:01 +0200

    ajout module exmaple avec requirejs et fichier requirejs

---
+++ app/design/frontend/Magento/luma/web/js/main.js
@@ -0,0 +1,12 @@
+define([
+        "jquery",
+    ],
+    function($) {
+        "use strict";
+
+        // Here your custom code...
+        alert('coucou');
+        console.log('Hola');
+
+        return;
+    });
